916183a208fc6d8aa2bc58542c25b4678f1b3d35
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-23 13:22:13 -0400

    Add Override ReduceLROnPlateau

--- torchconfig/optim.py
+++ torchconfig/optim.py
@@ -7,3 +7,6 @@ from torchconfig.filter import filter_args
 
 old_SGD = copy.deepcopy(optim.SGD)
 optim.SGD = lambda *args, **kwargs: old_SGD(*args, **filter_args(kwargs, old_SGD))
+
+old_ReduceLROnPlateau = copy.deepcopy(optim.lr_scheduler.ReduceLROnPlateau)
+optim.lr_scheduler.ReduceLROnPlateau = lambda *args, **kwargs: old_ReduceLROnPlateau(*args, **filter_args(kwargs, old_ReduceLROnPlateau))
